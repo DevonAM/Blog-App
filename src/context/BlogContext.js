645033e6c4a62cfd07e92cc29c01b86c14bb5c9d
Devon Acree-Meza <dacreeme@asu.edu>
2021-06-16 11:19:02 -0700

    add state to context

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 const BlogContext = React.createContext();
 
 export const BlogProvider = ({ children }) => {
-  <BlogContext.Provider>{children}</BlogContext.Provider>;
+  const [blogPosts, setBlogPosts] = useState([]);
+
+  const addBlogPost = () => {
+    setBlogPosts([
+      ...blogPosts,
+      { title: `Blog Post ${blogPosts.length + 1}` },
+    ]);
+  };
+
+  <BlogContext.Provider value={{ data: blogPosts, addBlogPost }}>
+    {children}
+  </BlogContext.Provider>;
 };
+
+export default BlogContext;
