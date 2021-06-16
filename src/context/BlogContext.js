0e676c885dabe54dd3939b8d43747efe118547fa
Devon Acree-Meza <dacreeme@asu.edu>
2021-06-16 10:43:12 -0700

    added context

---
+++ src/context/BlogContext.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BlogContext = React.createContext();
+
+export const BlogProvider = ({ children }) => {
+  <BlogContext.Provider>{children}</BlogContext.Provider>;
+};
