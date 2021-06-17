11ff69874365f98b80cce7afc5fd7a75f3084147
Devon Acree-Meza <dacreeme@asu.edu>
2021-06-17 09:45:07 -0700

    refactor to use reducers

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -2,14 +2,20 @@ import React, { useState } from "react";
 
 const BlogContext = React.createContext();
 
+const blogReducer = (state, action) => {
+  switch (action.type) {
+    case "add_blogpost":
+      return [...state, `Blog Post ${state.length + 1}`];
+    default:
+      return state;
+  }
+};
+
 export const BlogProvider = ({ children }) => {
-  const [blogPosts, setBlogPosts] = useState([]);
+  const [blogPosts, dispatch] = useReducer(blogReducer, []);
 
   const addBlogPost = () => {
-    setBlogPosts([
-      ...blogPosts,
-      { title: `Blog Post ${blogPosts.length + 1}` },
-    ]);
+    dispatch({ type: "add_blogpost" });
   };
 
   return (
