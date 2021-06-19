ba04d0440821706b5d6be5a5d3da34bd7ad3f3c5
Devon Acree-Meza <dacreeme@asu.edu>
2021-06-19 15:04:10 -0600

    add some styling for list of blogs, add delete icon

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { View, Text, StyleSheet, FlatList, Button } from "react-native";
 import { Context } from "../context/BlogContext";
+import { Feather } from "@expo/vector-icons";
 
 const IndexScreen = () => {
   const { state, addBlogPost } = useContext(Context);
@@ -12,11 +13,33 @@ const IndexScreen = () => {
         data={state}
         keyExtractor={(blogPost) => blogPost.title}
         renderItem={({ item }) => {
-          return <Text>{item.title}</Text>;
+          return (
+            <View style={styles.row}>
+              <Text style={styles.title}>{item.title}</Text>
+              <Feather style={styles.icon} name="trash"></Feather>
+            </View>
+          );
         }}
       />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  icon: {
+    fontSize: 25,
+  },
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    borderTopWidth: 1,
+    borderColor: "grey",
+    paddingVertical: 10,
+    paddingHorizontal: 10,
+  },
+  title: {
+    fontSize: 18,
+  },
+});
 
 export default IndexScreen;
