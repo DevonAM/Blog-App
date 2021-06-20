6157d216ae7e93f1de10f396c7df4233ca7782a3
Devon Acree-Meza <dacreeme@asu.edu>
2021-06-20 12:38:54 -0600

    add boilerplate for showscreen component

---
+++ src/screens/ShowScreen.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const ShowScreen = () => {
+  return <Text>Show Screen</Text>;
+};
+
+export default ShowScreen;
