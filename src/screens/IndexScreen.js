19a7a44d6d2e7242644580308b281773510e193f
Devon Acree-Meza <dacreeme@asu.edu>
2021-06-21 17:42:30 -0600

    make network request on use effect and add nav listener

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import {
   View,
   Text,
@@ -10,7 +10,21 @@ import { Context } from "../context/BlogContext";
 import { Feather } from "@expo/vector-icons";
 
 const IndexScreen = ({ navigation }) => {
-  const { state, deleteBlogPost } = useContext(Context);
+  const { state, deleteBlogPost , getBlogPosts} = useContext(Context);
+
+  useEffect(() => {
+    getBlogPosts();
+
+    //call blogposts when indexscreen comes back into focus
+    const listener = navigation.addListener('didFocus',()=>{
+      getBlogPosts();
+    });
+
+    //run this when IndexScreen COMPLETELY dismounts
+    return () => {
+      listener.remove();
+    }
+  },[]);
 
   return (
     <View>
