0aeda5e2f1f733b809238a021430427d9e6cec77
Devon Acree-Meza <dacreeme@asu.edu>
2021-06-20 21:35:06 -0600

    show clicked blogpost

--- src/screens/ShowScreen.js
+++ src/screens/ShowScreen.js
@@ -1,8 +1,16 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { useContext } from "react/cjs/react.development";
+import { Context } from "../context/BlogContext";
+const ShowScreen = ({ navigation }) => {
+  const { state } = useContext(Context);
 
-const ShowScreen = () => {
-  return <Text>Show Screen</Text>;
+  const blogPost = state.find((post) => post.id === navigation.getParam("id"));
+  return (
+    <Text>
+      {blogPost.title} - {blogPost.content}
+    </Text>
+  );
 };
 
 export default ShowScreen;
