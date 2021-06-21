479ec6f8397cf741d01977c263eeda2b71d06b76
Devon Acree-Meza <dacreeme@asu.edu>
2021-06-21 14:33:00 -0600

    create edit blogpost logic

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -3,6 +3,12 @@ import createDataContext from "./createDataContext";
 
 const blogReducer = (state, action) => {
   switch (action.type) {
+    case "edit_blogpost":
+      return state.map((post) => {
+        if (post.id === action.payload.id) {
+          return action.payload;
+        } else return post;
+      });
     case "delete_blogpost":
       return state.filter((blogPost) => blogPost.id !== action.payload);
     case "add_blogpost":
@@ -34,8 +40,14 @@ const deleteBlogPost = (dispatch) => {
   };
 };
 
+const editBlogPost = (dispatch) => {
+  return (id, title, content) => {
+    dispatch({ type: "edit_blogpost", payload: { id, title, content } });
+  };
+};
+
 export const { Context, Provider } = createDataContext(
   blogReducer,
-  { addBlogPost, deleteBlogPost },
+  { addBlogPost, deleteBlogPost, editBlogPost },
   []
 );
