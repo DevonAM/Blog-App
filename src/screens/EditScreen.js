7de7deced3eb9f5c9dcd5673d3a748a81bae0f1f
Devon Acree-Meza <dacreeme@asu.edu>
2021-06-21 14:32:43 -0600

    finish editscreen component.  navigate to index after submit

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -3,10 +3,19 @@ import { Text, TextInput, View, StyleSheet } from "react-native";
 import { Context } from "../context/BlogContext";
 import BlogPostForm from "../components/BlogPostForm";
 const EditScreen = ({ navigation }) => {
-  const { state } = useContext(Context);
-  const blogPost = state.find((post) => post.id === navigation.getParam("id"));
+  const id = navigation.getParam("id");
+  const { state, editBlogPost } = useContext(Context);
+  const blogPost = state.find((post) => post.id === id);
 
-  return <BlogPostForm />;
+  return (
+    <BlogPostForm
+      initialValues={{ title: blogPost.title, content: blogPost.content }}
+      onSubmit={(title, content) => {
+        editBlogPost(id, title, content);
+        navigation.navigate("Index");
+      }}
+    />
+  );
 };
 
 const styles = StyleSheet.create({});
